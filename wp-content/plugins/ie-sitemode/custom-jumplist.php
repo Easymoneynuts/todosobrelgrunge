b1d7e53191ede17202d49d2bbd81a93169dc95db
unknown <Lenovo@LENOVO-315-15ISK-1TBW10HOME.LENOVO-315-15ISK-1TBW10HOME>
2023-08-29 18:27:01 -0500

    primer commit

---
+++ wp-content/plugins/ie-sitemode/custom-jumplist.php
@@ -0,0 +1,2 @@
+function wpcom_custom_jumplist_reader(){window.external.msSiteModeCreateJumplist("Entradas recientes");window.external.msSiteModeAddJumpListItem("","https://todosobrelgrunge.wordpress.com/2023/08/29/hello-world/","https://s1.wp.com/i/favicon.ico");}
+try{if(window.external && 'msIsSiteMode' in window.external){if (window.external.msIsSiteMode()){wpcom_custom_jumplist_reader();wpcom_sitemode_comment_notify();}}}catch(e){}
